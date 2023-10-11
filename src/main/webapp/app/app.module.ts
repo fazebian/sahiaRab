eaeb132a747553ed9b1b173082ded34b1aaccb76
fazebian <rabariari.sa@gmail.com>
2023-10-11 09:11:44 +0300

    AJOUT DE FICHIER TEXTE POUR LA LICENSE.

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -20,6 +20,7 @@ import { httpInterceptorProviders } from 'app/core/interceptor/index';
 import MainComponent from './layouts/main/main.component';
 import MainModule from './layouts/main/main.module';
 import { AppPageTitleStrategy } from './app-page-title-strategy';
+import { TextFileComponent } from './text-file/text-file.component';
 
 @NgModule({
   imports: [
@@ -40,6 +41,7 @@ import { AppPageTitleStrategy } from './app-page-title-strategy';
     { provide: TitleStrategy, useClass: AppPageTitleStrategy },
   ],
   bootstrap: [MainComponent],
+  declarations: [TextFileComponent],
 })
 export class AppModule {
   constructor(applicationConfigService: ApplicationConfigService, iconLibrary: FaIconLibrary, dpConfig: NgbDatepickerConfig) {
